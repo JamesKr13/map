e959e8131578a4d036620ef2eefbc44f86f6226c
{James:Richards} <jamesckrichards@gmail.com>
2022-11-29 09:42:15 +1300

    Island
    Creates a single island

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 use ::rand::prelude::*;
 use macroquad::prelude::*;
 use noise::{NoiseFn, Perlin};
+
+
+fn min(one: f32,two: f32) -> f32{
+    match one < two {
+        true => one,
+        _ => two,
+    }
+} 
+fn abs(num: f32) -> f32 {
+    return num.powf(2.).sqrt()
+} 
+
 #[macroquad::main("map")]
 async fn main() {
     let mut perlin = Perlin::new((0..1000).choose(&mut thread_rng()).unwrap());
@@ -15,7 +27,8 @@ async fn main() {
                 let (x_pos, y_pos) = (x + base_x, y + base_y);
                 let xn = zoom * x_pos as f64 / width;
                 let yn = zoom * y_pos as f64 / height;
-                let val = perlin.get([xn, yn]);
+                let d = min(1.,((xn as f32).powf(2.)+(yn as f32).powf(2.))/(2. as f32).sqrt()) * 4.;
+                let val = perlin.get([xn, yn]) + ((1.-d)/2.) as f64;
                 if val > 0.7 {
                     draw_rectangle(x as f32, y as f32, 1., 1., LIGHTGRAY);
                 } else if val > 0.2 {
